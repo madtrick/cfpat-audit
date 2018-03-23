03120d82d5995d93cb1a4473a4dd18a6eced09b0
Farruco Sanjurjo <madtrick@gmail.com>
2018-03-23 15:16:23 +0100

    write leaked tokens to S3 from lambda function

--- handler.js
+++ handler.js
@@ -1,7 +1,19 @@
 'use strict';
 
+const Bluebird = require('bluebird');
+
 const findOrganizationCPATS = require('./lib/find-organization-cpats');
+const storeAuditLog = require('./lib/aws/store-audit-log');
 
 module.exports.run = (event, context, callback) => {
-  findOrganizationCPATS().catch(callback).then(callback);
+  Bluebird.coroutine(function * () {
+    try {
+      const tokens = yield findOrganizationCPATS();
+      yield storeAuditLog(JSON.stringify(tokens));
+
+      callback(null);
+    } catch (e) {
+      callback(e);
+    }
+  })();
 };
